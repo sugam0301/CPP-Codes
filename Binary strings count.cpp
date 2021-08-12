c484a10bc9e97c933bf7ef38f4fe8f4c89b86bbe
sugam0301 <66478859+sugam0301@users.noreply.github.com>
2021-08-12 23:11:43 +0530

    Add files via upload

---
+++ Binary strings count.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+int32_t main()
+{
+int t;cin>>t;
+while(t--)
+{	
+	int n; cin>>n;
+	int dp[n+1] = {-1};
+	dp[0]=0, dp[1] = 2, dp[2] =3; 
+
+	for(int i=3;i<=n;i++)
+	{
+		dp[i] = dp[i-1] + dp[i-2] ;	
+	}
+	cout<<dp[n]<<endl;
+
+}
+return 0;   
+
+}
