315b51b4628ac35a26c6141112bb9572e6ef650b
sugam0301 <66478859+sugam0301@users.noreply.github.com>
2021-08-23 21:43:49 +0530

    Update Pivot in sorted and rotated array.cpp

--- Pivot in sorted and rotated array.cpp
+++ Pivot in sorted and rotated array.cpp
@@ -8,24 +8,16 @@ int main()
         cin>>arr[i];
 
     int low=0,high=n-1,mid;
-    int res=arr[low];
-    while(low<=high)
+    while(low<high)
     {
         mid=(low+high)>>1;
        
         if(arr[mid]>=arr[low])
-        {
-            res=mid;
             low=mid+1;
-        }
         else if(arr[mid]<=arr[high])
-        {
             high=mid;
-        }
+        
 
     }
-   if(res>0)
-   		cout<<res-1;
-	else	
-		cout<<n-1;
+   cout<<arr[low];
 }
