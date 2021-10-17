f6d2fae0741e57dab2375a11e43b1ce2344e9606
sugam0301 <66478859+sugam0301@users.noreply.github.com>
2021-10-18 00:19:40 +0530

    Create House Robber - II.cpp

---
+++ House Robber - II.cpp
@@ -0,0 +1,21 @@
+int solve(vector<int> &nums , int curr, int dest,vector<int> &dp)
+    {       
+        if(curr>=dest) return 0; 
+        int tmp=nums.size();
+		
+        //Try Every jump 1 to nums[curr] jump
+        //and find minimum steps need to reach to end
+		if(dp[curr]!=-1) return dp[curr];
+        for(int i=1;i<=nums[curr];i++)
+        {
+            tmp=min(tmp,1+solve(nums,curr+i,dest,dp));  
+        } 
+        dp[curr]=tmp;
+         return tmp;  
+    }
+    
+    int jump(vector<int>& nums) 
+    {
+        vector<int> dp(nums.size(),-1);
+        return solve(nums,0,nums.size()-1,dp);   
+    }
